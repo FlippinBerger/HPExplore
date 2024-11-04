e8b3d4c9616255a302a63a90577c79efc4a434cd
Chris Berger <flippinberger@gmail.com>
2024-11-04 12:37:17 -0700

    Initial Commit

---
+++ HPExplore/HPExploreApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPExploreApp.swift
+//  HPExplore
+//
+//  Created by Chris Berger on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HPExploreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HPExploreTests/HPExploreTests.swift
@@ -0,0 +1,17 @@
+//
+//  HPExploreTests.swift
+//  HPExploreTests
+//
+//  Created by Chris Berger on 11/4/24.
+//
+
+import Testing
+@testable import HPExplore
+
+struct HPExploreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
